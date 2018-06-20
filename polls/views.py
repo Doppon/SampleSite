f6e26a0e1a2f2aaee85a2d26473b8825270a4a39
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-20 09:37:07 +0900

    detailのviewを追加

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello World")
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at questions %s" % question_id)
