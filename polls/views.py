2f5684a04247bfbf6ecac750f40bcda192b76fff
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-20 10:04:40 +0900

    indexのレスポンスを変更

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("Hello World")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse()
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at questions %s." % question_id)
